2602b26ae3a7a9ee1002956893cbc1bd6e3e1364
Captain-b <babakrezai@hotmail.com>
2023-01-31 14:51:42 +0000

    Create an empty class for our api calls and web socket calls.

---
+++ StockPrice/Utils/API/API.swift
@@ -0,0 +1,16 @@
+//
+//  API.swift
+//  StockPrice
+//
+//  Created by Babak Rezai on 31/01/2023.
+//
+
+class FinnHub {
+    class API {
+        
+    }
+    
+    class Socket {
+        
+    }
+}
