45ed4cd394c2ce24f269b9e575fd4f250f0d878b
Captain-b <babakrezai@hotmail.com>
2023-01-31 12:12:55 +0000

    Create our company info view controller.

---
+++ StockPrice/ViewControllers/CompanyInfoViewController/CompanyInfoViewController.swift
@@ -0,0 +1,12 @@
+//
+//  CompanyInfoViewController.swift
+//  StockPrice
+//
+//  Created by Babak Rezai on 31/01/2023.
+//
+
+import UIKit
+
+class CompanyInfoViewController: UIViewController {
+    
+}
