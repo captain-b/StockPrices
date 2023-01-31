951f4bf284cfab09bc6a04b414edf555d7014002
Captain-b <babakrezai@hotmail.com>
2023-01-31 16:52:57 +0000

    Add our home vc variables.

--- StockPrice/ViewControllers/HomeViewController/HomeViewController.swift
+++ StockPrice/ViewControllers/HomeViewController/HomeViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class HomeViewController: UIViewController {
     @IBOutlet weak var stocksTableView: UITableView!
+    internal let companies: [Ticker] = [.Google, .Apple, .Meta, .Tesla, .Amazon, .Coke]
+    internal var companyData = [Company]()
+    internal var tableViewCompanyData: [String: [Company]] = [:]
+    internal let socket = FinnHub.Socket()
+    internal let api = FinnHub.API()
+    internal var companyToOpen = Company()
     override func viewDidLoad() {
         super.viewDidLoad()
         setDelegates()
