aea60d68a4071723575544876ff72d9eefa4ab3e
Captain-b <babakrezai@hotmail.com>
2023-01-31 12:07:02 +0000

    Create our stocksTableView IBOutlet and connect it to the storyboard.

--- StockPrice/ViewControllers/HomeViewController/HomeViewController.swift
+++ StockPrice/ViewControllers/HomeViewController/HomeViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var stocksTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
     }
