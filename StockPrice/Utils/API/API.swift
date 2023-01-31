fabe80765789c9483f69024ffcd29646de7ab715
Captain-b <babakrezai@hotmail.com>
2023-01-31 14:54:01 +0000

    Add our FinnHub url constant and token.

--- StockPrice/Utils/API/API.swift
+++ StockPrice/Utils/API/API.swift
@@ -7,6 +7,8 @@
 
 class FinnHub {
     class API {
+        fileprivate let token = "cf93lraad3i9ljn7d4c0cf93lraad3i9ljn7d4cg"
+        fileprivate let url = "https://finnhub.io/api/v1"
         
     }
     
