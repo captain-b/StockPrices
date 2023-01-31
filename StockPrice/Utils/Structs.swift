68d53b90cfd31e6bc3a0c1f55ef3a6583a8aaf1d
Captain-b <babakrezai@hotmail.com>
2023-01-31 15:39:04 +0000

    Create a struct for our company data.

--- StockPrice/Utils/Structs.swift
+++ StockPrice/Utils/Structs.swift
@@ -22,6 +22,21 @@ enum CellIdentifier: String {
     case stockData = "stock_cell"
 }
 
+struct Company: Codable {
+    var country: String?
+    var currency: String?
+    var exchange: String?
+    var ipo: String?
+    var marketCapitalization: Double?
+    var name: String?
+    var phone: String?
+    var shareOutstanding: Double?
+    var ticker: String?
+    var weburl: String?
+    var logo: String?
+    var finnhubIndustry: String?
+}
+
 enum Ticker: String {
     /// Google
     case Google = "GOOGL"
