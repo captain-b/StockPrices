43098672e9c2bdbb5b84e3076ba3940e50560db6
Captain-b <babakrezai@hotmail.com>
2023-01-31 17:16:40 +0000

    Call our findCompanyData function from the home viewDidLoad method.

--- StockPrice/ViewControllers/HomeViewController/HomeViewController.swift
+++ StockPrice/ViewControllers/HomeViewController/HomeViewController.swift
@@ -18,6 +18,7 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setDelegates()
+        findCompanyData()
     }
     
     private func setDelegates() {
