1c1ba5018a8fb94e4dd4ef521597f56471199d51
Captain-b <babakrezai@hotmail.com>
2023-01-31 14:16:32 +0000

    Create a class for our company logos in the tableview cell.

--- StockPrice/Utils/UI/UIImageViews.swift
+++ StockPrice/Utils/UI/UIImageViews.swift
@@ -5,4 +5,17 @@
 //  Created by Babak Rezai on 31/01/2023.
 //
 
-import Foundation
+import UIKit
+
+class CompanyLogoImageView: UIImageView {
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setStyles()
+    }
+    
+    private func setStyles() {
+        layer.cornerRadius = frame.height / 2
+        clipsToBounds = true
+    }
+}
+
