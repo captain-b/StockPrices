be617fdb64306eed32d821c7d72bd2f137a2dbb6
Captain-b <babakrezai@hotmail.com>
2023-01-31 15:57:18 +0000

    Create a function to subscribe to our api socket events.

--- StockPrice/Utils/API/API.swift
+++ StockPrice/Utils/API/API.swift
@@ -86,7 +86,78 @@ class FinnHub {
     }
     
     class Socket: WebSocketDelegate {
+        private var _tickers = [Ticker]()
+        private let socket = WebSocket(request: URLRequest(url: URL(string: "wss://ws.finnhub.io?token=cf93lraad3i9ljn7d4c0cf93lraad3i9ljn7d4cg")!))
         
+        weak var delegate: FinnHubSocketDelegate?
+        
+        func subscribe(tickers: [Ticker]) {
+            _tickers = tickers
+            socket.delegate = self
+            socket.connect()
+        }
+        
+        func didReceive(event: Starscream.WebSocketEvent, client: Starscream.WebSocket) {
+            switch event {
+            case .connected(_):
+                _tickers.forEach { ticker in
+                    do {
+                        let req =  try JSONSerialization.data(withJSONObject: ["type": "subscribe", "symbol": ticker.rawValue])
+                        socket.write(string: String(data: req, encoding: .utf8) ?? "")
+                    } catch let myJSONError {
+                        print(myJSONError)
+                    }
+                }
+            case .disconnected(let data, _):
+                /**
+                 Notifies the delegate that the WebSocket connection has been disconnected, passing in the data received when disconnected.
+                 */
+                delegate?.disconnected(data: data)
+            case .text(let text):
+                do {
+                    let res = try JSONSerialization.jsonObject(with: text.data(using: .utf8)!) as? [String: Any] ?? [:]
+                    /**
+                     Notifies the delegate of new data received on the WebSocket connection by calling the 'websocket(updated:)' method on the delegate and passing in the data processed by the 'getResponseData(res:)' method.
+                     */
+                    delegate?.websocket(updated: getResponseData(res: res))
+                } catch {
+                    print(error)
+                }
+            case .error(let error):
+                /**
+                 Notifies the delegate of an error that occurred on the WebSocket connection by calling the 'websocket(error:)' method on the delegate and passing in the error.
+                 */
+                delegate?.websocket(error: error)
+            default:
+                break
+            }
+        }
+        
+        private func getResponseData(res: [String: Any]) -> LastPriceDataSubscription {
+            let data = res[LastPriceDataResponse.data.rawValue] as? NSArray ?? []
+            var subscriptions = [LastPriceData]()
+            data.forEach { item in
+                let item = item as? [String: Any] ?? [:]
+                let symbol = item[LastPriceResponse.symbol.rawValue] as? String ?? ""
+                let lastPrice = item[LastPriceResponse.lastPrice.rawValue] as? Double ?? 0
+                let timestamp = item[LastPriceResponse.timestamp.rawValue] as? Int64 ?? 0
+                let volume = item[LastPriceResponse.volume.rawValue] as? Double ?? 0
+                subscriptions.append(LastPriceData(
+                    symbol: symbol,
+                    lastPrice: lastPrice,
+                    timestamp: timestamp,
+                    volume: volume
+                ))
+            }
+            
+            let type = res[LastPriceDataResponse.type.rawValue] as? String ?? ""
+            let subscription = LastPriceDataSubscription(
+                type: type,
+                data: subscriptions
+            )
+            
+            return subscription
+        }
     }
 }
 
